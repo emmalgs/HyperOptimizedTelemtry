b98460c2d08209d571be0eb12193bb72b7195534
Emma Gerigscott <elgerig@gmail.com>
2023-11-08 09:57:06 -0800

    Implement optimized telemetry buffer

--- HyperOptimizedTelemetry.cs
+++ HyperOptimizedTelemetry.cs
@@ -4,7 +4,10 @@ public static class TelemetryBuffer
 {
     public static byte[] ToBuffer(long reading)
     {
-        throw new NotImplementedException("Please implement the static TelemetryBuffer.ToBuffer() method");
+        byte[] bytes = BitConverter.GetBytes(reading);
+        int prefix = 256 - bytes.Length;
+
+        return new byte[] { (byte)prefix, bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], bytes[7] };
     }
 
     public static long FromBuffer(byte[] buffer)
